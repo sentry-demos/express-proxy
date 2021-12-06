707402193b42722394cda763ffccf9c29dabf3e3
William Capozzoli <thinkocapo@gmail.com>
2021-12-06 14:16:39 -0800

    onProxyReq onProxyRes callbacks

--- server.js
+++ server.js
@@ -11,5 +11,24 @@ Sentry.init({ dsn: "https://bf57b19ffefb46efbef467dfc5f57de7@o87286.ingest.sentr
 // The request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler());
 
-app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ 
+    target: 'http://www.example.org', changeOrigin: true,
+    
+    onProxyReq: (proxyReq, req, res) => {
+        console.log("> onProxyReq - can we start a transaction here")
+        
+        // add custom header to request
+        proxyReq.setHeader('x-added', 'foobar');
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        console.log("> onProxyRes - can we finish the transaction here")
+
+        console.log("proxRes", proxyRes) // x-added is available on proxRes
+        // console.log("req", req)
+        // console.log("res", res)
+
+        // proxyRes.headers['x-added'] = 'foobar'; // add new header to response
+        // delete proxyRes.headers['x-removed']; // remove header from response
+    }
+}));
 app.listen(3000);
